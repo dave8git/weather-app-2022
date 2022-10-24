903b902cf08f19b6fa018024fefbb9be57c11b54
dave8git <dave8git@gmail.com>
2022-10-25 00:36:46 +0200

    showing temp city and icon works

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,12 +1,35 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import {useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+const [data, setData] = useState('');
+
+  const handleCityChange = useCallback(city => {  // dzięki użyciu hooka useCallback, odświeżenie komponentu WeatherBox nie będzie bez potrzeby odświżało komponentu <PickCity> 
+    console.log('PickCity city', city);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6987d79e2dd1eb2b4e3389157436e27&units=metric`)
+      .then(res => res.json())
+      .then(data => {
+        const weatherData = {
+          city: data.name, 
+          temp: data.main.temp, 
+          icon: data.weather[0].icon, 
+          description: data.weather[0].main
+        };
+        
+        setData(weatherData);
+        console.log('weatherData', weatherData);
+      });
+
+  });
+
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
+      <PickCity action={handleCityChange} />
+      <WeatherSummary data={data} />
       <Loader />
     </section>
   )
